7ce0fc539d8a84a970772e797f8873ef6a791f1a
Con <ckritikos@gmail.com>
2024-04-14 16:16:07 +1000

    Add additional logic to handle encryption/decryption choice Add comments

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 import enc_dec_funcs as edf
 
-plain_text = input("Enter your secret word: ")
-shift = int(input("Enter your shift value: "))
+# ask for whether the message needs to be encrypted or decrypted and store in variable
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt\n").lower()
 
-edf.encrypt(word=plain_text, encryption=shift)
+# ask for the word they use wants to encrypt/decrypt
+plain_text = input("Enter your secret word: ").lower()
+# ask the user for the shift value
+shift = int(input("Enter your shift value: \n"))
+
+# check if encryption or decryption method was chosen and run the appropriate function
+if direction == 'encode':
+    edf.encrypt(word=plain_text, encryption=shift)
