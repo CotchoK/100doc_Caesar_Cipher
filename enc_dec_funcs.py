84ef43d847f17a399495cac25cbd4d3464e2d2ed
Con <ckritikos@gmail.com>
2024-04-16 06:13:06 +1000

    Add logo. Add looping mechanism for re-running program on user input. Add character handling that is not alpha.

--- enc_dec_funcs.py
+++ enc_dec_funcs.py
@@ -14,28 +14,36 @@ def caesar(word, encryption, direction):
     # variable to store cipher_text we will build up
     resultant_text = ''
 
+    # if user enter a cipher key greater than 25 we always
+    # want to have the number in the range of our indexes in the alpha list
+    if encryption > 25:
+        encryption %= 25
+
     # for loop construct for the length of the word passed to it
     for pos_in_word in range(len(word)):
 
-        # store the index of the same letter from the alpha list
-        pos_in_alpha_list = alpha_list.alpha.index(word[pos_in_word])
+        # check if character is in list (encrypting only alpha characters, not numbers or symbols)
+        if word[pos_in_word] not in alpha_list.alpha:
+            resultant_text += word[pos_in_word]
+        else:
+            # store the index of the same letter from the alpha list
+            pos_in_alpha_list = alpha_list.alpha.index(word[pos_in_word])
 
-        # wrap around logic for shift mechanism
-        if direction == 'encode':
-            if pos_in_alpha_list + encryption > 25:
-                cipher_letter_pos = pos_in_alpha_list + encryption - 26
-                resultant_text += alpha_list.alpha[cipher_letter_pos]
-            else:
-                cipher_letter_pos = pos_in_alpha_list + encryption
-                resultant_text += alpha_list.alpha[cipher_letter_pos]
-        elif direction == 'decode':
-            if pos_in_alpha_list - encryption < 0:
-                cipher_letter_pos = pos_in_alpha_list - encryption + 26
-                resultant_text += alpha_list.alpha[cipher_letter_pos]
-            else:
-                cipher_letter_pos = pos_in_alpha_list - encryption
-                resultant_text += alpha_list.alpha[cipher_letter_pos]
-
+            # wrap around logic for shift mechanism
+            if direction == 'encode':
+                if pos_in_alpha_list + encryption > 25:
+                    cipher_letter_pos = pos_in_alpha_list + encryption - 26
+                    resultant_text += alpha_list.alpha[cipher_letter_pos]
+                else:
+                    cipher_letter_pos = pos_in_alpha_list + encryption
+                    resultant_text += alpha_list.alpha[cipher_letter_pos]
+            elif direction == 'decode':
+                if pos_in_alpha_list - encryption < 0:
+                    cipher_letter_pos = pos_in_alpha_list - encryption + 26
+                    resultant_text += alpha_list.alpha[cipher_letter_pos]
+                else:
+                    cipher_letter_pos = pos_in_alpha_list - encryption
+                    resultant_text += alpha_list.alpha[cipher_letter_pos]
 
     # print out the cipher text for testing
     print(resultant_text)
--- main.py
+++ main.py
@@ -1,12 +1,37 @@
 import enc_dec_funcs as edf
+import ascii_art as aa
 
-# ask for whether the message needs to be encrypted or decrypted and store in variable
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt\n").lower()
+# print the logo of the application
+print(aa.logo)
 
-# ask for the word they use wants to encrypt/decrypt
-text = input("Enter your secret word: ").lower()
-# ask the user for the shift value
-shift = int(input("Enter your shift value: \n"))
+def run():
+    # ask for whether the message needs to be encrypted or decrypted and store in variable
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt\n").lower()
 
-# run caesar function
-edf.caesar(word=text, encryption=shift, direction=direction)
+    # ask for the word they use wants to encrypt/decrypt
+    text = input("Enter your secret word: ").lower()
+    # ask the user for the shift value
+    shift = int(input("Enter your shift value: \n"))
+
+    # run caesar function
+    edf.caesar(word=text, encryption=shift, direction=direction)
+
+    re_run = input("Type 'yes' if you want to go again, otherwise type 'no'. ")
+    if re_run == 'yes':
+        run()
+    else:
+        print("Goodbye")
+        exit()
+
+
+# run program
+run()
+
+
+
+
+
+
+
+
+
